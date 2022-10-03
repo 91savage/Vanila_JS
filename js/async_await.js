dcae9a084de87f89ea64c1e4fe94b51e2baea57d
91savage <ekdma1403@gmail.com>
2022-10-04 01:26:24 +0900

    promise

---
+++ js/async_await.js
@@ -0,0 +1,9 @@
+function a() {
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+        });
+}
+
+a();
