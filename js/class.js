289156bb9a222ba2d4c5ed0da7e2ed88d75ce1e2
91Savage <ekdma1403@gmail.com>
2022-10-06 14:52:36 +0900

    class

--- js/class.js
+++ js/class.js
@@ -36,7 +36,22 @@ class Car {
 }
 
 let genesis = new Car('Genesis', '2022', 'gv70', 6400);
-let modelName = genesis.getPrice();
-console.log(modelName);
+let price = genesis.getPrice();
+console.log(price);
 genesis.setPrice(6660);
 console.log(genesis.getPrice());
+
+class ElectricCar extends Car {
+    constructor(modelName, modelYear, price, chargeTime) {
+        super(modelName, modelYear, 'e', price);
+        this.chargeTime = chargeTime;
+    }
+
+    setChargeTime(time) {
+        this.chargeTime = time;
+    }
+
+    getChargeTime() {
+        return this.chargeTime;
+    }
+}
